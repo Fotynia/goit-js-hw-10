0160bc4e2f9ebf0f8aff6fc61320f828bc098128
Svitlana Dyachok <korneyswet@gmail.com>
2023-04-03 16:23:12 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
+
 const countryInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
